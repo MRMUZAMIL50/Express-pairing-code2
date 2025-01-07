7e3d47d1fe18f90fd65b54f0cf6356acdf70d706
MRMUZAMIL50 <muzamilzakhmi687@gmail.com>
2025-01-07 16:50:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ async function startnigg(phone) {
           await xlicon.sendMessage(
             xlicon.user.id,
             {
-              text: 'HELLO THERE! 👋 \n\nDO NOT SHARE YOUR SESSION ID WITH ANYONE.\n\nPUT THE ABOVE IN SESSION_ID VAR\n\nTHANKS FOR USING SILENT-SOBX-MD BOT\n\n JOIN SUPPORT CHANNEL:-https://whatsapp.com/channel/0029VaHO5B0G3R3cWkZN970s \n',
+              text: 'HELLO THERE! 👋 \n\nDO NOT SHARE YOUR SESSION ID WITH ANYONE.\n\nPUT THE ABOVE IN SESSION_ID VAR\n\nTHANKS FOR USING MUZAMIL-ZAKHMI-MD BOT\n\n JOIN SUPPORT CHANNEL:-https://whatsapp.com/channel/0029VajJxbM4inotel0srJ2b \n',
             },
             { quoted: guru }
           )
