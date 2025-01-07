f76c8b6dc63ebdbb26734af86604065d9ee11c47
MRMUZAMIL50 <muzamilzakhmi687@gmail.com>
2025-01-07 17:23:02 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 import * as mega from 'megajs';
 
 const auth = {
-    email: 'muzamilmureedani12@gmail.com',
+    email: 'sobxmd@gmail.com',
     password: 'muzamilzakhmi@00',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
